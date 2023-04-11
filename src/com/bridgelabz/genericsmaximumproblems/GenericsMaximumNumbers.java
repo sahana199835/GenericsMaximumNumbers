54d826f1471d0035c067f80418ba2c0f8ae4bf33
sahana199835 <tnsahana1@gmail.com>
2023-04-11 15:34:53 +0530

    add:find maximum string value

--- src/com/bridgelabz/genericsmaximumproblems/GenericsMaximumNumbers.java
+++ src/com/bridgelabz/genericsmaximumproblems/GenericsMaximumNumbers.java
@@ -17,11 +17,27 @@ public class GenericsMaximumNumbers {
 		 return max;
 
 	 }
-	 public static void main(String[] args) {
+	 public static void main1(String[] args) {
 		System.out.println (maximum(5, 6, 1));
 		System.out.println ( "Maximum Float Value is : " +maximum(5.4, 6.9, 1.42));
 
 	 }
+}
+
+public static <A extends Comparable<A>> A maximum(A x, A y, A z) {
+    A max = x;
+    if (y.compareTo(max) > 0) {
+        max = y;
+    }
+    if (z.compareTo(max) > 0) {
+        max = z;
+    }
+    return max;
+}
+
+public static void main(String[] args) {
+    System.out.println("Maximum String Value is : " + maximum("A", "K", "X"));
+}
 }
 
 
