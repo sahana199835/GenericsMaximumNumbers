74856158385925dc2a99d06e7d85ebc57bafeb73
sahana199835 <tnsahana1@gmail.com>
2023-04-11 15:23:11 +0530

    add: .gitignore and README.md files added

---
+++ src/com/bridgelabz/genericsmaximumproblems/GenericsMaximumNumbers.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.genericsmaximumproblems;
+
+public class GenericsMaximumNumbers {
+	
+	public static <A extends Comparable<A>> A maximum (A x, A y, A z){
+
+		 A max = x;
+
+		 if (y.compareTo(max) > 0) {
+			 max = y;
+
+		 }
+		 if (z.compareTo(max)>0) {
+			 max = z;
+		 }
+
+		 return max;
+
+	 }
+	 public static void main(String[] args) {
+		System.out.println (maximum(5, 6, 1));
+	 }
+}
+
+
+
+
