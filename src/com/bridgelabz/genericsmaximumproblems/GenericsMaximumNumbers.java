d5c167311ccd8f6a4e32046e6bcdb0f5218b42d1
sahana199835 <tnsahana1@gmail.com>
2023-04-11 15:41:42 +0530

    add:check extending maximum method

--- src/com/bridgelabz/genericsmaximumproblems/GenericsMaximumNumbers.java
+++ src/com/bridgelabz/genericsmaximumproblems/GenericsMaximumNumbers.java
@@ -2,42 +2,34 @@ package com.bridgelabz.genericsmaximumproblems;
 
 public class GenericsMaximumNumbers {
 	
-	public static <A extends Comparable<A>> A maximum (A x, A y, A z){
+	public static <A extends Comparable<A>> A maximum(A x, A y, A z, A u, A v) {
+		A max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		if (u.compareTo(max) > 0) {
+			max = u;
+		}
+		if (v.compareTo(max) > 0) {
+			max = v;
+		}
 
-		 A max = x;
 
-		 if (y.compareTo(max) > 0) {
-			 max = y;
+		  System.out.println("Maximum value: " + max);
+		return max;
 
-		 }
-		 if (z.compareTo(max)>0) {
-			 max = z;
-		 }
 
-		 return max;
+	}
 
-	 }
-	 public static void main1(String[] args) {
-		System.out.println (maximum(5, 6, 1));
-		System.out.println ( "Maximum Float Value is : " +maximum(5.4, 6.9, 1.42));
+	public static void main(String[] args) {
 
-	 }
-}
+		System.out.println("Maximum String Value : " + maximum('A', 'K', 'S', 'T', 'G'));
+		System.out.println("Maximum String Value : " + maximum("Ant", "King", "Spider", "Thor", "Yoyo"));
+	}
 
-public static <A extends Comparable<A>> A maximum(A x, A y, A z) {
-    A max = x;
-    if (y.compareTo(max) > 0) {
-        max = y;
-    }
-    if (z.compareTo(max) > 0) {
-        max = z;
-    }
-    return max;
-}
-
-public static void main(String[] args) {
-    System.out.println("Maximum String Value is : " + maximum("A", "K", "X"));
-}
 }
 
 
